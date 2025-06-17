9f12a5e2a12f2fa1d37dfe5967781b46681d5dac
ychalady <todyannawut@gmail.com>
2025-06-17 03:08:20 +0000

    negga

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+number = input(": ")
+number = int(number)
+if number == 0:
+    print("This number is equal to zero.")
+if number < 0:
+    print("this number is negative.")
+if number > 0:
+    print("this number is positive.")
+print(" ")
